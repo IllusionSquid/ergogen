38b49b37fdaee6f806a162a1a68818eb50b99dc9
IllusionSquid <theillusionsquid@gmail.com>
2023-01-23 16:16:10 +0100

    start on making things options

--- src/footprints/choc_diode.js
+++ src/footprints/choc_diode.js
@@ -22,9 +22,12 @@ module.exports = {
     hotswap: false,
     reverse: false,
     keycaps: false,
-    diode: false
+    diode: false,
+    diode_silks: true,
+    rgb: false
   },
   body: p => {
+    const RGB_Offset = 4.7;
     const standard = `
       (module PG1350 (layer F.Cu) (tedit 5DD50112)
       ${p.at /* parametric position */}
@@ -59,17 +62,38 @@ module.exports = {
       (fp_line (start 9 8.5) (end -9 8.5) (layer Dwgs.User) (width 0.15))
       (fp_line (start -9 8.5) (end -9 -8.5) (layer Dwgs.User) (width 0.15))
       `
+    
+    const key_silks = `
+    ${''/* Key coord silks */}
+    (fp_text user "(R${p.net.to.name.substring(3)}, C${p.net.from.name.substring(3)})" (at 0 2.54 ${p.rot} unlocked) (layer "F.SilkS")
+      (effects (font (size 0.75 0.75) (thickness 0.15)))
+    )
+    (fp_text user "(R${p.net.to.name.substring(3)}, C${p.net.from.name.substring(3)})" (at 0 2.54 ${p.rot} unlocked) (layer "B.SilkS")
+      (effects 
+        (font (size 0.75 0.75) (thickness 0.15))
+        (justify mirror)
+      )
+    )
+    `
+    
+    function diode(reversible, silks) {
+      let template = `
+      ${''/* SMD pads on both sides */}
+      (pad 2 smd rect (at 8 3.65 ${p.rot-90}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.local_net('Diode').str})
+      (pad 3 smd rect (at 8 0.35 ${p.rot-90}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.net.to.str})
+      `
 
-    const diode = `
-        
+      if (reversible)
+        template += `
+        (pad 2 smd rect (at 8 3.65 ${p.rot-90}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.local_net('Diode').str})
+        (pad 3 smd rect (at 8 0.35 ${p.rot-90}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.net.to.str})
+        (pad 3 thru_hole circle (at 8 1.5) (size 0.6 0.6) (drill 0.3) (layers *.Cu) (zone_connect 2) ${p.net.to.str})
+        (pad 2 thru_hole circle (at 8 2.5) (size 0.6 0.6) (drill 0.3) (layers *.Cu) (zone_connect 2) ${p.local_net('Diode').str})
+        `
+      
+      if (silks) {
+        template += `
         ${''/* diode symbols */}
-        (fp_line (start 8 2.25) (end 8 2.75) (layer F.SilkS) (width 0.1))
-        (fp_line (start 8.4 2.25) (end 8 1.65) (layer F.SilkS) (width 0.1))
-        (fp_line (start 7.6 2.25) (end 8.4 2.25) (layer F.SilkS) (width 0.1))
-        (fp_line (start 8 1.65) (end 7.6 2.25) (layer F.SilkS) (width 0.1))
-        (fp_line (start 8 1.65) (end 8.55 1.65) (layer F.SilkS) (width 0.1))
-        (fp_line (start 8 1.65) (end 7.45 1.65) (layer F.SilkS) (width 0.1))
-        (fp_line (start 8 1.25) (end 8 1.65) (layer F.SilkS) (width 0.1))
         (fp_line (start 8 2.25) (end 8 2.75) (layer B.SilkS) (width 0.1))
         (fp_line (start 8.4 2.25) (end 8 1.65) (layer B.SilkS) (width 0.1))
         (fp_line (start 7.6 2.25) (end 8.4 2.25) (layer B.SilkS) (width 0.1))
@@ -77,144 +101,148 @@ module.exports = {
         (fp_line (start 8 1.65) (end 8.55 1.65) (layer B.SilkS) (width 0.1))
         (fp_line (start 8 1.65) (end 7.45 1.65) (layer B.SilkS) (width 0.1))
         (fp_line (start 8 1.25) (end 8 1.65) (layer B.SilkS) (width 0.1))
-       
-        ${''/* SMD pads on both sides */}
-        (pad 3 smd rect (at 8 0.35 ${p.rot-90}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.net.to.str})
-        (pad 2 smd rect (at 8 3.65 ${p.rot-90}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.local_net('Diode').str})
-        (pad 3 smd rect (at 8 0.35 ${p.rot-90}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.net.to.str})
-        (pad 2 smd rect (at 8 3.65 ${p.rot-90}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.local_net('Diode').str})
-
-        (pad 3 thru_hole circle (at 8 1.5) (size 0.6 0.6) (drill 0.3) (layers *.Cu) (zone_connect 2) ${p.net.to.str})
-        ${''/*(fp_line (start 8 1.5) (end 8 0.35) (width 0.25) (layer "F.Cu"))
-        (fp_line (start 8 1.5) (end 8 0.35) (width 0.25) (layer "B.Cu"))*/}
-        (pad 2 thru_hole circle (at 8 2.5) (size 0.6 0.6) (drill 0.3) (layers *.Cu) (zone_connect 2) ${p.local_net('Diode').str})
-        ${''/*(fp_line (start 8 2.5) (end 8 3.65) (width 0.25) (layer "F.Cu"))
-        (fp_line (start 8 2.5) (end 8 3.65) (width 0.25) (layer "B.Cu"))
-        (pad 3 smd custom (at 8 0.75 ${p.rot + 180}) (size 0.25 1) (layers F.Cu)
-        (zone_connect 0)
-        (options (clearance outline) (anchor rect))
-        (primitives
-        ))*/}
-        
-        ${''/* Diode SMD Traces */}
-        (pad 2 smd custom (at 8 2.5 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.local_net('Diode').str}
-          (zone_connect 0)
-          (options (clearance outline) (anchor circle))
-          (primitives
-            (gr_line (start 0 0) (end 0 1.15) (width 0.25))
-          )
-        )
-        (pad 2 smd custom (at 8 2.5 ${p.rot}) (size 0.25 0.25) (layers B.Cu) ${p.local_net('Diode').str}
-          (zone_connect 0)
-          (options (clearance outline) (anchor circle))
-          (primitives
-            (gr_line (start 0 0) (end 0 1.15) (width 0.25))
-          )
-        )
-
-        (pad 3 smd custom (at 8 1.5 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.net.to.str}
-          (zone_connect 0)
-          (options (clearance outline) (anchor circle))
-          (primitives
-            (gr_line (start 0 0) (end 0 -1.15) (width 0.25))
-          )
-        )
-        (pad 3 smd custom (at 8 1.5 ${p.rot}) (size 0.25 0.25) (layers B.Cu) ${p.net.to.str}
-          (zone_connect 0)
-          (options (clearance outline) (anchor circle))
-          (primitives
-            (gr_line (start 0 0) (end 0 -1.15) (width 0.25))
-          )
-        )
-
-        
-        ${''/* Column SMD Traces */}
-        (pad 1 smd custom (at 3.275 -5.95 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.net.from.str}
-          (zone_connect 0)
-          (options (clearance outline) (anchor circle))
-          (primitives
-            (gr_line (start 0 0) (end -0.5 0.5) (width 0.25))
-            (gr_line (start -0.5 0.5) (end -0.5 7.525) (width 0.25))
-            (gr_line (start -0.5 7.525) (end 1.725 9.75) (width 0.25))
-          )
-        )
-        (pad 1 smd custom (at -3.275 -5.95 ${p.rot}) (size 0.25 0.25) (layers B.Cu) ${p.net.from.str}
-          (zone_connect 0)
-          (options (clearance outline) (anchor circle))
-          (primitives
-            (gr_line (start 0 0) (end 0.5 0.5) (width 0.25))
-            (gr_line (start 0.5 0.5) (end 0.5 7.525) (width 0.25))
-            (gr_line (start 0.5 7.525) (end -1.725 9.75) (width 0.25))
-          )
-        )
-        (pad 1 smd custom (at -5 3.8 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.net.from.str}
-          (zone_connect 0)
-          (options (clearance outline) (anchor circle))
-          (primitives
-            (gr_line (start 0 0) (end 10 0) (width 0.25))
-          )
-        )
-
-        ${''/* "Inner" SMD Traces */}
-        (pad 2 smd custom (at -8.275 -3.75 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.local_net('Diode').str}
-          (zone_connect 0)
-          (options (clearance outline) (anchor circle))
-          (primitives
-            (gr_line (start 0 0) (end 0 6.4) (width 0.25))
-            (gr_line (start 0 6.4) (end 3.25 9.65) (width 0.25))
-            (gr_line (start 3.25 9.65) (end 8.275 9.65) (width 0.25))
-          )
-        )
-        (pad 2 smd custom (at 0 5.9 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.local_net('Diode').str}
-          (zone_connect 0)
-          (options (clearance outline) (anchor circle))
-          (primitives
-            (gr_line (start 0 0) (end 5.75 0) (width 0.25))
-            (gr_line (start 5.75 0) (end 8 -2.25) (width 0.25))
-          )
-        )
-        (pad 2 smd custom (at 8.275 -3.75 ${p.rot}) (size 0.25 0.25) (layers B.Cu) ${p.local_net('Diode').str}
-          (zone_connect 0)
-          (options (clearance outline) (anchor circle))
-          (primitives
-            (gr_line (start 0 0) (end -1.5 1.5) (width 0.25))
-            (gr_line (start -1.5 1.5) (end -1.5 5.025) (width 0.25))
-            (gr_line (start -1.5 5.025) (end -0.275 6.25) (width 0.25))
-          )
-        )
-
-        ${''/* Key coord silks */}
-        (fp_text user "(R${p.net.to.name.substring(3)}, C${p.net.from.name.substring(3)})" (at 0 2.54 ${p.rot} unlocked) (layer "F.SilkS")
-          (effects (font (size 0.75 0.75) (thickness 0.15)))
-        )
-        (fp_text user "(R${p.net.to.name.substring(3)}, C${p.net.from.name.substring(3)})" (at 0 2.54 ${p.rot} unlocked) (layer "B.SilkS")
-          (effects 
-            (font (size 0.75 0.75) (thickness 0.15))
-            (justify mirror)
-          )
-        )
         `
+        
+        if (reversible)
+        template += `
+        (fp_line (start 8 2.25) (end 8 2.75) (layer F.SilkS) (width 0.1))
+        (fp_line (start 8.4 2.25) (end 8 1.65) (layer F.SilkS) (width 0.1))
+        (fp_line (start 7.6 2.25) (end 8.4 2.25) (layer F.SilkS) (width 0.1))
+        (fp_line (start 8 1.65) (end 7.6 2.25) (layer F.SilkS) (width 0.1))
+        (fp_line (start 8 1.65) (end 8.55 1.65) (layer F.SilkS) (width 0.1))
+        (fp_line (start 8 1.65) (end 7.45 1.65) (layer F.SilkS) (width 0.1))
+        (fp_line (start 8 1.25) (end 8 1.65) (layer F.SilkS) (width 0.1))
+        `
+      }
+        
+
+      return template
+    } 
+
+    const diode_traces = `
+    ${''/* Diode SMD Traces */}
+    (pad 2 smd custom (at 8 2.5 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.local_net('Diode').str}
+      (zone_connect 0)
+      (options (clearance outline) (anchor circle))
+      (primitives
+        (gr_line (start 0 0) (end 0 1.15) (width 0.25))
+      )
+    )
+    (pad 2 smd custom (at 8 2.5 ${p.rot}) (size 0.25 0.25) (layers B.Cu) ${p.local_net('Diode').str}
+      (zone_connect 0)
+      (options (clearance outline) (anchor circle))
+      (primitives
+        (gr_line (start 0 0) (end 0 1.15) (width 0.25))
+      )
+    
+    (pad 3 smd custom (at 8 1.5 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.net.to.str}
+      (zone_connect 0)
+      (options (clearance outline) (anchor circle))
+      (primitives
+        (gr_line (start 0 0) (end 0 -1.15) (width 0.25))
+      )
+    )
+    (pad 3 smd custom (at 8 1.5 ${p.rot}) (size 0.25 0.25) (layers B.Cu) ${p.net.to.str}
+      (zone_connect 0)
+      (options (clearance outline) (anchor circle))
+      (primitives
+        (gr_line (start 0 0) (end 0 -1.15) (width 0.25))
+      )
+    
+    
+    ${''/* Column SMD Traces */}
+    (pad 1 smd custom (at 3.275 -5.95 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.net.from.str}
+      (zone_connect 0)
+      (options (clearance outline) (anchor circle))
+      (primitives
+        (gr_line (start 0 0) (end -0.5 0.5) (width 0.25))
+        (gr_line (start -0.5 0.5) (end -0.5 7.525) (width 0.25))
+        (gr_line (start -0.5 7.525) (end 1.725 9.75) (width 0.25))
+      )
+    )
+    (pad 1 smd custom (at -3.275 -5.95 ${p.rot}) (size 0.25 0.25) (layers B.Cu) ${p.net.from.str}
+      (zone_connect 0)
+      (options (clearance outline) (anchor circle))
+      (primitives
+        (gr_line (start 0 0) (end 0.5 0.5) (width 0.25))
+        (gr_line (start 0.5 0.5) (end 0.5 7.525) (width 0.25))
+        (gr_line (start 0.5 7.525) (end -1.725 9.75) (width 0.25))
+      )
+    )
+    (pad 1 smd custom (at -5 3.8 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.net.from.str}
+      (zone_connect 0)
+      (options (clearance outline) (anchor circle))
+      (primitives
+        (gr_line (start 0 0) (end 10 0) (width 0.25))
+      )
+    
+    ${''/* "Inner" SMD Traces */}
+    (pad 2 smd custom (at -8.275 -3.75 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.local_net('Diode').str}
+      (zone_connect 0)
+      (options (clearance outline) (anchor circle))
+      (primitives
+        (gr_line (start 0 0) (end 0 6.4) (width 0.25))
+        (gr_line (start 0 6.4) (end 3.25 9.65) (width 0.25))
+        (gr_line (start 3.25 9.65) (end 8.275 9.65) (width 0.25))
+      )
+    )
+    (pad 2 smd custom (at 0 5.9 ${p.rot}) (size 0.25 0.25) (layers F.Cu) ${p.local_net('Diode').str}
+      (zone_connect 0)
+      (options (clearance outline) (anchor circle))
+      (primitives
+        (gr_line (start 0 0) (end 5.75 0) (width 0.25))
+        (gr_line (start 5.75 0) (end 8 -2.25) (width 0.25))
+      )
+    )
+    (pad 2 smd custom (at 8.275 -3.75 ${p.rot}) (size 0.25 0.25) (layers B.Cu) ${p.local_net('Diode').str}
+      (zone_connect 0)
+      (options (clearance outline) (anchor circle))
+      (primitives
+        (gr_line (start 0 0) (end -1.5 1.5) (width 0.25))
+        (gr_line (start -1.5 1.5) (end -1.5 5.025) (width 0.25))
+        (gr_line (start -1.5 5.025) (end -0.275 6.25) (width 0.25))
+      )
+    )
+    `
+
+    const rgb = `
+    (fp_poly (pts (xy 2.8 ${-1.4 + RGB_Offset}) (xy 2.2 ${-1.4 + RGB_Offset}) (xy 2.2 ${-2 + RGB_Offset})) (layer F.SilkS) (width 0.1))
+    (fp_line (start 1.6 ${-1.4 + RGB_Offset}) (end 1.6 ${1.4 + RGB_Offset}) (layer Cmts.User) (width 0.12))
+    (fp_line (start 1.6 ${1.4 + RGB_Offset}) (end -1.6 ${1.4 + RGB_Offset}) (layer Cmts.User) (width 0.12))
+    (fp_line (start -1.6 ${1.4 + RGB_Offset}) (end -1.6 ${-1.4 + RGB_Offset}) (layer Cmts.User) (width 0.12))
+    (fp_line (start -1.6 ${-1.4 + RGB_Offset}) (end 1.6 ${-1.4 + RGB_Offset}) (layer Cmts.User) (width 0.12))
+    (fp_line (start 1.7 ${-1.5 + RGB_Offset}) (end 1.7 ${1.5 + RGB_Offset}) (layer Edge.Cuts) (width 0.12))
+    (fp_line (start 1.7 ${1.5 + RGB_Offset}) (end -1.7 ${1.5 + RGB_Offset}) (layer Edge.Cuts) (width 0.12))
+    (fp_line (start -1.7 ${1.5 + RGB_Offset}) (end -1.7 ${-1.5 + RGB_Offset}) (layer Edge.Cuts) (width 0.12))
+    (fp_line (start -1.7 ${-1.5 + RGB_Offset}) (end 1.7 ${-1.5 + RGB_Offset}) (layer Edge.Cuts) (width 0.12))
+    (pad "1" smd rect (at -2.55 ${0.75+ RGB_Offset} ${p.rot}) (size 1.7 0.82) (layers "F.Cu" "F.Paste" "F.Mask"))
+    (pad "2" smd rect (at -2.55 ${-0.75+ RGB_Offset} ${p.rot}) (size 1.7 0.82) (layers "F.Cu" "F.Paste" "F.Mask"))
+    (pad "4" smd rect (at 2.55 ${0.75+ RGB_Offset} ${p.rot}) (size 1.7 0.82) (layers "F.Cu" "F.Paste" "F.Mask"))
+    (pad "3" smd roundrect (at 2.55 ${-0.75+ RGB_Offset} ${p.rot}) (size 1.7 0.82) (layers "F.Cu" "F.Paste" "F.Mask") (roundrect_rratio 0.25))
+        
+    `
 
     function pins(def_neg, def_pos, def_side) {
       return `
-        ${'' /* holes */}
-        (pad "" np_thru_hole circle (at ${def_pos}5 -3.75) (size 3 3) (drill 3) (layers *.Cu *.Mask))
-        (pad "" np_thru_hole circle (at 0 -5.95) (size 3 3) (drill 3) (layers *.Cu *.Mask))
-    
-        ${'' /* net pads */}
-        (pad 1 smd rect (at ${def_neg}3.275 -5.95 ${p.rot}) (size 2.6 2.6) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask)  ${p.net.from.str})
-        (pad 2 smd rect (at ${def_pos}8.275 -3.75 ${p.rot}) (size 2.6 2.6) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask)  ${p.local_net('Diode').str})
+      ${'' /* holes */}
+      (pad "" np_thru_hole circle (at ${def_pos}5 -3.75) (size 3 3) (drill 3) (layers *.Cu *.Mask))
+      (pad "" np_thru_hole circle (at 0 -5.95) (size 3 3) (drill 3) (layers *.Cu *.Mask))
+  
+      ${'' /* net pads */}
+      (pad 1 smd rect (at ${def_neg}3.275 -5.95 ${p.rot}) (size 2.6 2.6) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask)  ${p.net.from.str})
+      (pad 2 smd rect (at ${def_pos}8.275 -3.75 ${p.rot}) (size 2.6 2.6) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask)  ${p.local_net('Diode').str})
 
-          ${''/* pins */}
-          (pad 1 thru_hole circle (at ${def_pos}5 3.8) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.net.from.str})
-          (pad 2 thru_hole circle (at ${def_pos}0 5.9) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.local_net('Diode').str})
-        `
+      ${''/* pins */}
+      (pad 1 thru_hole circle (at ${def_pos}5 3.8) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.net.from.str})
+      (pad 2 thru_hole circle (at ${def_pos}0 5.9) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.local_net('Diode').str})
+      `
     }
+
     if(p.param.reverse) {
       return `
         ${standard}
         ${p.param.keycaps ? keycap : ''}
-        ${p.param.diode ? diode : ''}
+        ${p.param.diode ? diode(p.param.reverse, p.param.diode_silks) : ''}
         ${pins('-', '', 'B')}
         ${pins('', '-', 'F')})
         `
@@ -222,7 +250,7 @@ module.exports = {
       return `
         ${standard}
         ${p.param.keycaps ? keycap : ''}
-        ${p.param.diode ? diode : ''}
+        ${p.param.diode ? diode(p.param.reverse, p.param.diode_silks) : ''}
         ${pins('-', '', 'B')})
         `
     }
